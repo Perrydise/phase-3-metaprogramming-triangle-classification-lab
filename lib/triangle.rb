0a6cb5d78395af54a43e823a393298b6611edf14
Perry Jorss <pjorss123@gmail.com>
2022-12-04 16:06:19 -0500

    commit tree a48b52c8ed69a73b61642f8382ffc22c97d8fb42

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
   # write code here
+  attr_accessor :equilateral
+  attr_accessor :isosceles
+  attr_accessor :scalene
+  attr_accessor :kind
+
+  def initialize (equilateral, isosceles, scalene)
+      @equilateral = equilateral
+      @isosceles = isosceles
+    @scalene = scalene
+  end
+
+  def kind
+    
+  end
+  # class TriangleError < standardError
+  #   def
+
+  #   end
+  # end
 end
